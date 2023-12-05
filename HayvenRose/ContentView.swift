34cd5567fd550f138409cc96a61771dd1c5c8f1b
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-12-05 11:54:37 -0600

    Initial Commit

---
+++ HayvenRose/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HayvenRose
+//
+//  Created by J. DeWeese on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HayvenRose/HayvenRoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HayvenRoseApp.swift
+//  HayvenRose
+//
+//  Created by J. DeWeese on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct HayvenRoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
